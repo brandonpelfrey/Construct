97f58dc0b85b79a46506140d1542cf7a82c247a1
Brandon Pelfrey <brandonpelfrey@gmail.com>
2012-07-17 09:18:54 -0400

    Added lineIntegral operator

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	auto I = identity() / length(identity());
-	auto Z = grad(I);
 	Domain D = Domain(64,64,64, Vec3(-1,-1,-1), Vec3(1,1,1));
 	auto Ig = writeToGrid(I, constant(Vec3(0,0,0)), D);
 
-  cout << Z.eval(Vec3(1,2,3)) << endl;
+	auto li = lineIntegral(I, identity(), identity() - constant(Vec3(0,0,0)), constant(1.f), constant(.1f));
+
+  cout << I.eval(Vec3(-1,-1,-1)) << endl;
+	cout << Ig.eval(Vec3(-1,-1,-1)) << endl;
   return 0;
 }
